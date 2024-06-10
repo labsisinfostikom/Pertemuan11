3a70f1743a456747704e347816ea34914d4eeef3
Dosen <labsisteminformasistikom@gmail.com>
2024-06-10 21:01:13 +0700

    Commit Pertama

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// Numbers int & Float
+$age=11;
+echo $age;
+echo "<br>";
+$movie_rating = 3.2;
+echo $movie_rating;
+// Operator Aritmatika
+echo 5+1,"<br>";
+echo 6.5+1.2,"<br>";
+echo 19826-263,"<br>";
+echo -22.8 - 19.1,"<br>";
