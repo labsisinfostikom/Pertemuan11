6b0b9077cfd71dd77acb1f841632d9b4ad704dbd
labsisinfostikom <labsisteminformasistikom@gmail.com>
2024-06-10 21:11:02 +0700

    second Commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Numbers int & Float
-$age=11;
+$age=11.8;
 echo $age;
 echo "<br>";
 $movie_rating = 3.2;
@@ -10,3 +10,5 @@ echo 5+1,"<br>";
 echo 6.5+1.2,"<br>";
 echo 19826-263,"<br>";
 echo -22.8 - 19.1,"<br>";
+// Using Number Variables
+echo $age + $movie_rating, "<br>";
